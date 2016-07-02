dfeb22611cee11e2993a41703fab62bce9821ec4
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-02 14:40:57 +0530

    Upload application

---
+++ MVC_Movie/Controllers/IntroductoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class IntroductoryController : Controller
+    {
+        // GET: HelloWorld
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: HelloWorld/Welcome
+        public String Welcome()
+        {
+            return "This is the Welcome action method.";
+        }
+        public String Display(String name,int id)
+        {
+            return HttpUtility.HtmlEncode("hello, "+name+ ". number is "+id);
+        }
+    }
+}
